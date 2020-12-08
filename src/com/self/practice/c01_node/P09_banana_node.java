4449bcd984aef0f5a62b0504282be96e7e2c0872
chengtufeiyang <617581984@qq.com>
2020-12-08 14:28:17 +0800

    确定两个单向链表是否相交（第八条基础上：1、容器法，2、不用容器；情况：都无环，都有环，不可能存在一个有环一个无环的情况） 具体方法存放再四个内部类中

--- src/com/self/practice/c01_node/P09_banana_node.java
+++ src/com/self/practice/c01_node/P09_banana_node.java
@@ -23,20 +23,19 @@ public class P09_banana_node {
 
         //  both  no loop
         if (!P08_loop_node_entry_point.loopNode(headOne) && !P08_loop_node_entry_point.loopNode(headTwo)) {
-            return noLoopBananaNodeIsTrue(headOne,null,headTwo,null);
+            return NoLoopNodeBananaByContainer.noLoopBananaNodeByContainereIsTrue(headOne, null, headTwo, null);
         }
-
         // both loop
         if (P08_loop_node_entry_point.loopNode(headOne) && P08_loop_node_entry_point.loopNode(headTwo)) {
-            return loopBananaNodeIsTrue(headOne,P08_loop_node_entry_point.loopNodeEntryPointNode(headOne),
-                    headTwo,P08_loop_node_entry_point.loopNodeEntryPointNode(headTwo));
+
+            return LoopNodeBananaByContainer.loopBananaNodeByContainereIsTrue(headOne,headTwo);
         }
         return false;
     }
 
 
     /**
-     * 返回两个链表相交节点
+     * 返回两个链表相交节点---容器法
      *
      * @param headOne
      * @param headTwo
@@ -46,175 +45,159 @@ public class P09_banana_node {
 
         //  both  no loop
         if (!P08_loop_node_entry_point.loopNode(headOne) && !P08_loop_node_entry_point.loopNode(headTwo)) {
-            return noLoopBananaNode(headOne,null,headTwo,null);
+            return NoLoopNodeBananaByContainer.noLoopBananaNodeByContainere(headOne, null, headTwo, null);
         }
-
         // both loop
         if (P08_loop_node_entry_point.loopNode(headOne) && P08_loop_node_entry_point.loopNode(headTwo)) {
-            return loopBananaNode(headOne,P08_loop_node_entry_point.loopNodeEntryPointNode(headOne),
-                    headTwo,P08_loop_node_entry_point.loopNodeEntryPointNode(headTwo));
+            return LoopNodeBananaByContainer.loopBananaNodeByContainere(headOne, headTwo);
         }
         return null;
     }
 
-    /**
-     * 判断两个无环链表是否相交---非容器法
-     *
-     * @param headOne
-     * @param headTwo
-     * @return
-     */
-    public static boolean noLoopBananaNodeIsTrue(Node headOne,Node endNodeOne, Node headTwo,Node endNodeTwo) {
-
-        Node endOne = null;
-        Node endTwo = null;
-        Node temp_one = headOne;
-        int count = 0;
-        while (temp_one != endNodeOne) {//循环第一个
-            count++;
-            endOne = temp_one;
-            temp_one = temp_one.next;
-        }
-
-        Node temp_two = headTwo;
-        while (temp_two != endNodeTwo) {
-            count--;
-            endTwo = temp_two;
-            temp_two = temp_two.next;
-        }
-        //若count > 0 则链表 headOne较长   若count < 0 则链表headTwo 较长
-        return endOne == endTwo ? true : false;
-    }
 
     /**
-     * 判断两个有环链表是否相交---非容器法
-     *
-     * @param headOne
-     * @param headTwo
-     * @return
+     * 无环链表相交情况判断及返回相交节点---容器法
      */
-    public static boolean loopBananaNodeIsTrue(Node headOne,Node endNodeOne, Node headTwo,Node endNodeTwo) {
+    private static class NoLoopNodeBananaByContainer {
 
 
-       if (endNodeOne ==endNodeTwo){//入环节点相同情况
-            return noLoopBananaNodeIsTrue(headOne,endNodeOne,headTwo,endNodeTwo);
-       }else {//入环节点不同情况
-            Node fast = endNodeOne.next;
-            Node slow = endNodeTwo.next;
-            while (fast != endNodeOne && slow != endNodeTwo){
-                if (fast == slow){
+        /**
+         * 判断两个无环链表是否相交---容器法
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static boolean noLoopBananaNodeByContainereIsTrue(Node headOne, Node endNodeOne, Node headTwo, Node endNodeTwo) {
+            if (headOne == null && headTwo == null) return false;
+            if (headOne == null && headTwo != null) return false;
+            if (headOne != null && headTwo == null) return false;
+
+            Set<Node> setOne = new HashSet<>();//节存储容器
+            Node tempOne = headOne;
+            while (tempOne != endNodeOne) {
+                setOne.add(tempOne);
+                tempOne = tempOne.next;
+            }
+
+            Node tempTwo = headTwo;
+            while (tempTwo != endNodeTwo) {
+                if (setOne.contains(tempTwo)) {
                     return true;
                 }
-                slow = slow.next;
-                fast = fast.next.next;
+                tempTwo = tempTwo.next;
             }
             return false;
-       }
-    }
-
-    /**
-     * 判断两个无环链表是否相交---容器法
-     *
-     * @param headOne
-     * @param headTwo
-     * @return
-     */
-    public static boolean noLoopBananaNodeByContainereIsTrue(Node headOne,Node endNodeOne, Node headTwo,Node endNodeTwo) {
-        if (headOne==null && headTwo == null) return false;
-        if (headOne == null && headTwo != null) return false;
-        if (headOne != null && headTwo == null) return false;
-
-        Set<Node> setOne = new HashSet<>();
-        Node tempOne = headOne;
-        while (tempOne != endNodeOne){
-            setOne.add(tempOne);
-            tempOne = tempOne.next;
         }
 
-        Node tempTwo = headTwo;
-        while (tempTwo != endNodeTwo){
+        /**
+         * 返回两个无环链表相交节点---容器法
+         * 存在返回节点，不存在则返回null
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static Node noLoopBananaNodeByContainere(Node headOne, Node endNodeOne, Node headTwo, Node endNodeTwo) {
 
-            if (setOne.contains(tempTwo)){
-                return true;
+            if (headOne == null && headTwo == null) return null;
+            if (headOne == null && headTwo != null) return null;
+            if (headOne != null && headTwo == null) return null;
+
+            Set<Node> setOne = new HashSet<>();
+            Node tempOne = headOne;
+            while (tempOne != endNodeOne) {
+                setOne.add(tempOne);
+                tempOne = tempOne.next;
             }
-            tempTwo = tempTwo.next;
-        }
-        return false;
-    }
 
-
-
-    /**
-     * 返回两个无环链表相交节点---非容器法
-     *存在返回节点，不存在则返回null
-     * @param headOne
-     * @param headTwo
-     * @return
-     */
-    public static Node noLoopBananaNode(Node headOne,Node endNodeOne, Node headTwo,Node endNodeTwo) {
-
-        Node endOne = null;
-        Node endTwo = null;
-        Node temp_one = headOne;
-        int count = 0;
-        while (temp_one != endNodeOne) {//循环第一个
-            count++;
-            endOne = temp_one;
-            temp_one = temp_one.next;
-        }
-
-        Node temp_two = headTwo;
-        while (temp_two != endNodeTwo) {
-            count--;
-            endTwo = temp_two;
-            temp_two = temp_two.next;
-        }
-
-        //首先判断是否存在交点
-        if(endOne != endTwo){
-            return null;
-        }
-
-        //若count > 0 则链表 headOne较长   若count < 0 则链表headTwo 较长
-        Node longNode = count > 0 ? headOne:headTwo;
-        Node shortNode = longNode==headOne ? headTwo :headOne;
-
-        count = Math.abs(count);
-        while (count != 0){
-            longNode = longNode.next;
-        }
-
-        while (longNode !=endNodeTwo && shortNode != endNodeTwo){
-            if (longNode==shortNode){
-                return longNode;
-            }
-            longNode = longNode.next;
-            shortNode = shortNode.next;
-        }
-        return null;
-    }
-
-
-    /**
-     * 返回两个有环链表相交节点---非容器法
-     *存在返回节点，不存在则返回null
-     * @param headOne
-     * @param headTwo
-     * @return
-     */
-    public static Node loopBananaNode(Node headOne,Node endNodeOne, Node headTwo,Node endNodeTwo) {
-
-        if (endNodeOne ==endNodeTwo){//入环节点相同情况
-            return noLoopBananaNode(headOne,endNodeOne,headTwo,endNodeTwo);
-        }else {//入环节点不同情况
-            Node fast = endNodeOne.next;
-            Node slow = endNodeTwo.next;
-            while (fast != endNodeOne && slow != endNodeTwo){
-                if (fast == slow){
-                    return fast;
+            Node tempTwo = headTwo;
+            while (tempTwo != endNodeTwo) {
+                if (setOne.contains(tempTwo)) {
+                    return tempTwo;
                 }
-                slow = slow.next;
-                fast = fast.next.next;
+                tempTwo = tempTwo.next;
+            }
+            return null;
+        }
+    }
+
+    /**
+     * 无环链表相交情况判断及返回相交节点---非容器法
+     */
+    private static class NoLoopNodeBananaBySelf {
+        /**
+         * 判断两个无环链表是否相交
+         *根据最后一个节点是否相等判断两个无环链表
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static boolean noLoopBananaNodeIsTrue(Node headOne, Node endNodeOne, Node headTwo, Node endNodeTwo) {
+
+            Node endOne = null;
+            Node endTwo = null;
+            Node temp_one = headOne;
+            while (temp_one != endNodeOne) {
+                endOne = temp_one;
+                temp_one = temp_one.next;
+            }
+
+            Node temp_two = headTwo;
+            while (temp_two != endNodeTwo) {
+                endTwo = temp_two;
+                temp_two = temp_two.next;
+            }
+            return endOne == endTwo ? true : false;
+        }
+
+        /**
+         * 返回两个无环链表相交节点---非容器法
+         * 存在返回节点，不存在则返回null
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static Node noLoopBananaNode(Node headOne, Node endNodeOne, Node headTwo, Node endNodeTwo) {
+
+            Node endOne = null;
+            Node endTwo = null;
+            Node temp_one = headOne;
+            int count = 0;
+            while (temp_one != endNodeOne) {//循环第一个
+                count++;
+                endOne = temp_one;
+                temp_one = temp_one.next;
+            }
+
+            Node temp_two = headTwo;
+            while (temp_two != endNodeTwo) {
+                count--;
+                endTwo = temp_two;
+                temp_two = temp_two.next;
+            }
+
+            //首先判断是否存在交点
+            if (endOne != endTwo) {
+                return null;
+            }
+
+            //若count > 0 则链表 headOne较长   若count < 0 则链表headTwo 较长
+            Node longNode = count > 0 ? headOne : headTwo;
+            Node shortNode = longNode == headOne ? headTwo : headOne;
+
+            count = Math.abs(count);
+            while (count != 0) {
+                longNode = longNode.next;
+            }
+
+            while (longNode != null && shortNode != null) {
+                if (longNode == shortNode) {
+                    return longNode;
+                }
+                longNode = longNode.next;
+                shortNode = shortNode.next;
             }
             return null;
         }
@@ -222,39 +205,142 @@ public class P09_banana_node {
 
 
     /**
-     * 返回两个无环链表相交节点---容器法
-     *存在返回节点，不存在则返回null
-     * @param headOne
-     * @param headTwo
-     * @return
+     * 有环链表相交情况判断及返回相交节点---容器法
      */
-    public static Node noLoopBananaNodeByContainere(Node headOne, Node headTwo) {
+    private static class LoopNodeBananaByContainer {
 
-        if (headOne==null && headTwo == null) return null;
-        if (headOne == null && headTwo != null) return null;
-        if (headOne != null && headTwo == null) return null;
 
-        Set<Node> setOne = new HashSet<>();
-        Node tempOne = headOne;
-        while (tempOne != null){
-            setOne.add(tempOne);
-            tempOne = tempOne.next;
-        }
+        /**
+         * 判断两个有环链表是否相交---容器法
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static boolean loopBananaNodeByContainereIsTrue(Node headOne, Node headTwo) {
+            if (headOne == null && headTwo == null) return false;
+            if (headOne == null && headTwo != null) return false;
+            if (headOne != null && headTwo == null) return false;
 
-        Node tempTwo = headTwo;
-        while (tempTwo != null){
-            if (setOne.contains(tempTwo)){
-                return tempTwo;
+            Set<Node> setOne = new HashSet<>();//节存储容器
+            Node tempOne = headOne;
+            while (tempOne != null) {
+                if(!setOne.contains(tempOne)){
+                    setOne.add(tempOne);
+                }else {
+                    break;
+                }
+                tempOne = tempOne.next;
             }
-            tempTwo = tempTwo.next;
+
+            Node tempTwo = headTwo;
+            Set<Node> setTwo = new HashSet<>();//节存储容器
+            while (tempTwo != null) {
+                if (setOne.contains(tempTwo)) {
+                    return true;
+                }
+                if (!setTwo.contains(tempTwo)){
+                    setTwo.add(tempTwo);
+                }else {
+                    break;
+                }
+                tempTwo = tempTwo.next;
+            }
+            return false;
         }
-        return null;
+
+        /**
+         * 返回两个无环链表相交节点---容器法
+         * 存在返回节点，不存在则返回null
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static Node loopBananaNodeByContainere(Node headOne, Node headTwo) {
+
+            if (headOne == null && headTwo == null) return null;
+            if (headOne == null && headTwo != null) return null;
+            if (headOne != null && headTwo == null) return null;
+
+            Set<Node> setOne = new HashSet<>();
+            Node tempOne = headOne;
+            while (tempOne != null) {
+                if(!setOne.contains(tempOne)){
+                    setOne.add(tempOne);
+                }else {
+                    break;
+                }
+                tempOne = tempOne.next;
+            }
+
+            Node tempTwo = headTwo;
+            Set<Node> setTwo = new HashSet<>();//节存储容器
+            while (tempTwo != null) {
+                if (setOne.contains(tempTwo)) {
+                    return tempTwo;
+                }
+                if (!setTwo.contains(tempTwo)){
+                    setTwo.add(tempTwo);
+                }else {
+                    break;
+                }
+                tempTwo = tempTwo.next;
+            }
+            return null;
+        }
     }
 
+    /**
+     * 有环链表相交情况判断及返回相交节点---非容器法
+     */
+    private static class LoopNodeBananaBySelf {
+        /**
+         * 判断两个有环链表是否相交---非容器法
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static boolean loopBananaNodeIsTrue(Node headOne, Node endNodeOne, Node headTwo, Node endNodeTwo) {
 
+            if (endNodeOne == endNodeTwo) {//入环节点相同情况
+                return  NoLoopNodeBananaBySelf.noLoopBananaNodeIsTrue(headOne, endNodeOne, headTwo, endNodeTwo);
+            } else {//入环节点不同情况
+                Node fast = endNodeOne.next;
+                while (fast != endNodeOne){
+                    if (fast == endNodeTwo){
+                        return true;
+                    }
+                    fast = fast.next;
+                }
+                return false;
+            }
+        }
 
+        /**
+         * 返回两个有环链表相交节点---非容器法
+         * 存在返回节点，不存在则返回null
+         *
+         * @param headOne
+         * @param headTwo
+         * @return
+         */
+        public static Node loopBananaNode(Node headOne, Node endNodeOne, Node headTwo, Node endNodeTwo) {
 
-
-
+            if (endNodeOne == endNodeTwo) {//入环节点相同情况
+                return NoLoopNodeBananaBySelf.noLoopBananaNode(headOne, endNodeOne, headTwo, endNodeTwo);
+            } else {//入环节点不同情况
+                Node fast = endNodeOne.next;
+                while (fast != endNodeOne){
+                    if (fast == endNodeTwo){
+                        return fast;
+                    }
+                    fast = fast.next;
+                }
+                return null;
+            }
+        }
+    }
 
 }
