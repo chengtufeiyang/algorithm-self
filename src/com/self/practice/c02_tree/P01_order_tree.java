167b6ebeada0c78c2a86485b9b91dbb16c85a539
chengtufeiyang <617581984@qq.com>
2020-12-23 17:58:12 +0800

    前序、中序、后序遍历（递归、非递归方式） 缺失后序遍历非递归方式一个栈实现方式

---
+++ src/com/self/practice/c02_tree/P01_order_tree.java
@@ -0,0 +1,186 @@
+package com.self.practice.c02_tree;
+
+import com.self.practice.comm_tools.Constants;
+import com.self.practice.comm_tools.Tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class P01_order_tree {
+
+    private static RecusionOrder recusionOrder = new RecusionOrder();
+    private static NoRecusionOrder noRecusionOrder = new NoRecusionOrder();
+
+
+    public static void main(String[] args) {
+
+        Tree tree = Tree.createTree(10);
+        System.out.println("-------前序遍历-------");
+        order(tree,true,0);
+        System.out.println();
+        order(tree,false,0);
+        System.out.println();
+        System.out.println("-------中序遍历-------");
+        order(tree,true,1);
+        System.out.println();
+        order(tree,false,1);
+        System.out.println();
+        System.out.println("-------后序遍历-------");
+        order(tree,true,2);
+        System.out.println();
+        order(tree,false,2);
+
+
+    }
+
+    /**
+     * 排序
+     * @param tree  源数据
+     * @param flag  true---递归方式，false---非递归方式
+     * @param type  0---前序遍历，1---中序遍历，2---后序遍历
+     */
+    public static void order(Tree tree,boolean flag,int type){
+
+        if (flag){//判断是否为递归遍历方式
+            if (type==0)  recusionOrder.preOrder(tree);
+            if (type==1)  recusionOrder.midOrder(tree);
+            if (type==2)  recusionOrder.postOrder(tree);
+        }else {
+            if (type==0)  noRecusionOrder.preOrder(tree);
+            if (type==1)  noRecusionOrder.midOrder(tree);
+            if (type==2)  noRecusionOrder.postOrder(tree);
+        }
+    }
+
+
+
+
+    /**
+     * 递归遍历方式
+     */
+    private static class RecusionOrder{
+        /**
+         * 前序遍历---递归
+         * 中-左-右
+         * @param tree
+         */
+        public void preOrder(Tree tree){
+            if (null == tree) return;
+
+            System.out.print(tree.value+ Constants.SPACE);
+            preOrder(tree.left);
+            preOrder(tree.right);
+        }
+
+        /**
+         * 中序遍历---递归
+         * 左-中-右
+         * @param tree
+         */
+        public void midOrder(Tree tree){
+            if (null == tree) return;
+            midOrder(tree.left);
+            System.out.print(tree.value+ Constants.SPACE);
+            midOrder(tree.right);
+        }
+
+        /**
+         * 后序遍历---递归
+         * 左-右-中
+         * @param tree
+         */
+        public void postOrder(Tree tree){
+            if (null == tree) return;
+            postOrder(tree.left);
+            postOrder(tree.right);
+            System.out.print(tree.value+ Constants.SPACE);
+        }
+
+    }
+
+    /**
+     * 非递归遍历方式
+     */
+    private static class NoRecusionOrder{
+
+        /**
+         * 前序遍历---非递归
+         * 中-左-右
+         * @param tree
+         */
+        public void preOrder(Tree tree){
+            if (null == tree) return;
+            Deque<Tree> stack = new ArrayDeque<>();
+            stack.addLast(tree);
+            while (!stack.isEmpty()){
+                Tree temp = stack.pollLast();
+                System.out.print(temp.value+ Constants.SPACE);
+                if (null != temp.right){
+                    stack.addLast(temp.right);
+                }
+                if (null != temp.left){
+                    stack.addLast(temp.left);
+                }
+            }
+        }
+
+
+        /**
+         * 中序遍历---非递归
+         * 左-中-右
+         * @param tree
+         */
+        public void midOrder(Tree tree){
+            if (null == tree) return;
+            Deque<Tree> stack = new ArrayDeque<>();
+            prepareMid(stack,tree);
+
+            while (!stack.isEmpty()){
+                System.out.print(stack.pollLast().value+ Constants.SPACE);
+            }
+        }
+
+
+        public void prepareMid(Deque<Tree> stack,Tree tree){
+            if (null == tree) return;
+            if (null != tree.right){
+                prepareMid(stack,tree.right);
+            }
+            stack.addLast(tree);
+            if (null != tree.left){
+                prepareMid(stack,tree.left);
+            }
+
+        }
+
+
+        /**
+         * 后序遍历---非递归
+         * 左-右-中
+         * @param tree
+         */
+        public void postOrder(Tree tree){
+            if (null == tree) return;
+            Deque<Tree> stack = new ArrayDeque<>();
+            Deque<Tree> result = new ArrayDeque<>();
+            stack.addLast(tree);
+            while (!stack.isEmpty()){
+                Tree temp = stack.pollLast();
+                result.addLast(temp);
+                if (null != temp.left){
+                    stack.addLast(temp.left);
+                }
+                if (null != temp.right){
+                    stack.addLast(temp.right);
+                }
+            }
+
+            while (!result.isEmpty()){
+                System.out.print(result.pollLast().value+ Constants.SPACE);
+            }
+        }
+
+    }
+
+
+}
