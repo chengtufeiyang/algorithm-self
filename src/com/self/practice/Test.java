527b1f10ffce9127c2a4472b981ebd9cf32b2135
chengtufeiyang <617581984@qq.com>
2021-03-09 10:36:48 +0800

    测试优先级队列使用

--- src/com/self/practice/Test.java
+++ src/com/self/practice/Test.java
@@ -1,8 +1,12 @@
 package com.self.practice;
 
+import com.self.practice.comm_tools.Constants;
 import com.self.practice.comm_tools.Node;
 import com.self.practice.comm_tools.Tree;
 import com.wz.array.PrintArray;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Test {
     public static void main(String[] args) {
@@ -19,6 +23,23 @@ public class Test {
 //        Tree tree = Tree.createTree(4);
 //        Tree.prePrint(tree);
 
+
+        int [] arr = {2,4,2,7,9,1};
+        PriorityQueue<Integer> self = new PriorityQueue<>(
+//                (a,b) -> (b - a)
+                new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return o1 - o2;
+                    }
+                }
+        );
+        for (int i = 0; i < arr.length; i++) {
+            self.add(arr[i]);
+        }
+        while (!self.isEmpty()){
+            System.out.print(self.poll() + Constants.SPACE);
+        }
 
     }
 }
