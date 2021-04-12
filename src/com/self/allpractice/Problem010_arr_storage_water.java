0a3c6d2f1cf6e3a8fa3f24b27fbf5d9ca33e1710
chengtufeiyang <617581984@qq.com>
2021-04-12 13:05:03 +0800

    二维数组蓄水问题

--- src/com/self/allpractice/Problem010_arr_storage_water.java
+++ src/com/self/allpractice/Problem010_arr_storage_water.java
@@ -1,5 +1,8 @@
 package com.self.allpractice;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Problem010_arr_storage_water {
 
     public static void main(String[] args) {
@@ -86,4 +89,95 @@ public class Problem010_arr_storage_water {
         }
         return ans;
     }
+
+
+    private static class MaxWaterForTwoClass{
+
+        private int row;
+        private int col;
+        private int value;
+
+        public MaxWaterForTwoClass(int row, int col, int value) {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+        }
+    }
+
+    private static class MyCompare implements Comparator<MaxWaterForTwoClass>{
+
+        @Override
+        public int compare(MaxWaterForTwoClass o1, MaxWaterForTwoClass o2) {
+            return o1.value - o2.value;// 从小到达顺序
+        }
+    }
+
+    /**
+     * 二维数组蓄水问题
+     * @param arr
+     * @return
+     */
+    public static int maxWaterForTwo(int[][] arr){
+        if (null == arr || arr.length <3 || arr[0].length < 3) return 0;
+
+        int row = arr.length;
+        int col = arr[0].length;
+        boolean[][] flag = new boolean[row][col];  // 标记数据是否已入队列
+        //首先将外围数据存入小根堆中
+        PriorityQueue<MaxWaterForTwoClass> queue = new PriorityQueue<>(new MyCompare());
+        for (int i = 0; i < col; i++) { // up down
+            flag[0][i] = true;
+            queue.add(new MaxWaterForTwoClass(0,i,arr[0][i]));
+            flag[row - 1][i] = true;
+            queue.add(new MaxWaterForTwoClass(row - 1,i,arr[row - 1][i]));
+        }
+
+        for (int i = 0; i < row; i++) {  // left right
+            if (!flag[i][0]){
+                flag[i][0] = true;
+                queue.add(new MaxWaterForTwoClass(i,0,arr[i][0]));
+            }
+
+            if (!flag[i][col - 1]){
+                flag[i][col - 1] = true;
+                queue.add(new MaxWaterForTwoClass(i,col - 1,arr[i][col - 1]));
+            }
+        }
+
+        int max = 0;
+        int limit = Integer.MIN_VALUE;
+        while (!queue.isEmpty()){
+            MaxWaterForTwoClass temp = queue.poll(); //瓶颈
+            int tempRow = temp.row;
+            int tempCol = temp.col;
+            limit = Math.max(limit,temp.value);
+            //对四个方向数据进行判断 赋值
+            if (!flag[tempRow - 1][tempCol]){
+                flag[tempRow - 1][tempCol] = true;
+                max += Math.max(0,limit - arr[tempRow - 1][tempCol]);
+                queue.add(new MaxWaterForTwoClass(tempRow - 1,col,arr[tempRow - 1][tempCol]));
+            }
+
+            if (!flag[tempRow + 1][tempCol]){
+                flag[tempRow + 1][tempCol] = true;
+                max += Math.max(0,limit - arr[tempRow + 1][tempCol]);
+                queue.add(new MaxWaterForTwoClass(tempRow + 1,col,arr[tempRow + 1][tempCol]));
+            }
+
+            if (!flag[tempRow][tempCol - 1]){
+                flag[tempRow][tempCol - 1] = true;
+                max += Math.max(0,limit - arr[tempRow][tempCol - 1]);
+                queue.add(new MaxWaterForTwoClass(tempRow ,col - 1,arr[tempRow][tempCol - 1]));
+            }
+
+            if (!flag[tempRow][tempCol + 1]){
+                flag[tempRow][tempCol + 1] = true;
+                max += Math.max(0,limit - arr[tempRow][tempCol + 1]);
+                queue.add(new MaxWaterForTwoClass(tempRow ,col + 1,arr[tempRow][tempCol + 1]));
+            }
+        }
+        return max;
+    }
+
+
 }
