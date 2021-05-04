361e05b77752bf19965b9ecb4e3852ab50836153
chengtufeiyang <617581984@qq.com>
2021-05-04 11:06:29 +0800

    leetcode-7，利用取余，从为部开始获取每一个位置的数据，

---
+++ src/com/self/leetcode/Problem000007_submissions.java
@@ -0,0 +1,34 @@
+package com.self.leetcode;
+
+public class Problem000007_submissions {
+    public static void main(String[] args) {
+        Problem000007_submissions test = new Problem000007_submissions();
+        System.out.println(Integer.MAX_VALUE);
+        int x = 1534236469;
+        System.out.println(test.reverse(x));
+        System.out.println(test.reverse2(x));
+    }
+
+
+//    https://leetcode-cn.com/problems/reverse-integer/submissions/
+    public int reverse(int x) {
+        long result = 0;
+        while (x != 0 ){
+            result = result * 10 + x % 10;
+
+            x /= 10;
+        }
+        return (int)result == result ? (int)result : 0;
+    }
+
+
+    public int reverse2(int x) {
+        long res = 0;
+        while(x!=0){
+            res = res*10 +x%10;
+            x /=10;
+        }
+        return (int)res==res ? (int)res:0;
+    }
+
+}
